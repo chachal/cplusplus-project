3729a2817ee0f07e8286dc917f831142fa440f5e
Alén Charlotta <alenc1@turkoosi.org.aalto.fi>
2016-12-16 00:59:39 +0200

    Better pictures

--- src/birds.cpp
+++ src/birds.cpp
@@ -2,11 +2,12 @@
 #include "birds.hpp"
 
 void Bird::init(){
-  bird.setSize(sf::Vector2f(66.0f, 56.0f));
+  bird.setSize(sf::Vector2f(70.0f, 70.0f));
   bird.setRotation(0.f);
   bird.setOrigin(33.f, 28.f);
   bird.setPosition(0,0);
   birdTexture.loadFromFile("Sprites/box.png");
+  birdTexture.setSmooth(true);
   bird.setTexture(&birdTexture);
     
 }
@@ -22,6 +23,7 @@ void Bird2::init(){
   bird.setOrigin(25.f, 25.f);
   bird.setPosition(0,0);
   birdTexture.loadFromFile("Sprites/red.png");
+  birdTexture.setSmooth(true);
   bird.setTexture(&birdTexture);
 
 }
@@ -32,11 +34,12 @@ void Bird2::updatepos(sf::Vector2f pos){
 
 
 void Pig::init(){
-  bird.setRadius(28.0f);
+  bird.setRadius(29.0f);
   bird.setRotation(0.f);
   bird.setOrigin(28.f, 28.f);
   bird.setPosition(0,0);
-  birdTexture.loadFromFile("Sprites/red.png");
+  birdTexture.loadFromFile("Sprites/pig.png");
+  birdTexture.setSmooth(true);
   bird.setTexture(&birdTexture);
 }
 
